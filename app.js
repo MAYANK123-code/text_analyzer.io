36ff802a9d9630f80ab06d4a89fec24e898e46bb
Mayank Biswas <65966643+MAYANK123-code@users.noreply.github.com>
2020-11-29 19:49:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// Word Count:
+function wordCount() {
+	$()
+}
+
+
+// Unique Word Count:
+function unique() {
+	$()
+}
+
+/*
+function mostFrequentWord(words) {
+  var wordFrequencies = {};
+  for (var i = 0; i <= words.length; i++) {
+    if (words[i] in wordFrequencies) {
+      wordFrequencies[words[i]]++;
+    }
+    else {
+      wordFrequencies[words[i]]=1;
+    }
+  }
+  var currentMaxKey = Object.keys(wordFrequencies)[0];
+  var currentMaxCount = wordFrequencies[currentMaxKey];
+  
+  for (var word in wordFrequencies) {
+    if (wordFrequencies[word] > currentMaxCount) {
+      currentMaxKey = word;
+      currentMaxCount = wordFrequencies[word];
+    }
+  }
+  return currentMaxKey;
+}
+*/
+
+
+// Average Character length:
+function average() {
+	$()
+}
+
+/*
+function average(numbers) {
+  var total = numbers[0];
+  for (i = 0; i < numbers.length; i++) { 
+    total += numbers[i];
+  }                      
+  return total/numbers.length;
+}
+*/
+
+
+// Average Sentence length:
+function sentence() {
+	$()
+}
+
+
+$(document).ready(function() {
+	$('button').on('submit', function() {
+		
+
+	});
+});
